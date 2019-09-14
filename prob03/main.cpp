eb40465851e32bd6f39572650e326a23c5a71a52
Leobardo.arevalo25 <leobardo.arevalo25@cs104u-3b.acad.fullerton.edu>
2019-09-14 14:49:49 -0700

    done with prob03

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,13 @@
 // Sales prediction
+#include <iostream>
+
+int main(){
+ const int TOTAL = 2103419277;
+ const double PERCENT = 0.18;
+ //finding the percent by which sales have increased in 2019
+ long prediction = (TOTAL * PERCENT) + TOTAL;
+
+ std::cout << "Last year's sales were $" << TOTAL << std::endl;
+ std::cout << "this years sales prediction $" << prediction << std::endl;
+
+}
